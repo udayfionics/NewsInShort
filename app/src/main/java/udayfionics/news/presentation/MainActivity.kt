3387243458e3e2f5404c1bcecb87ca5753c8c9bd
udayfionics <udayvemps@gmail.com>
2023-05-06 15:25:25 +0530

    NavigateUp added

--- app/src/main/java/udayfionics/news/presentation/MainActivity.kt
+++ app/src/main/java/udayfionics/news/presentation/MainActivity.kt
@@ -1,12 +1,24 @@
 package udayfionics.news.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import udayfionics.news.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!.findNavController()
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
